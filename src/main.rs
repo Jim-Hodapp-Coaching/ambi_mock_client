cf051a74e9379568fd959165d8ac45d763f1a724
Caleb Bourg <calebbourg2@gmail.com>
2022-01-09 15:06:01 -0800

    add first pass at data generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,150 @@
+use rand::{thread_rng, Rng};
+use serde::{Serialize, Deserialize};
+use std::fmt;
+
+#[derive(Serialize, Deserialize)]
+struct Reading {
+  tempurature: String,
+  humidity: String,
+  pressure: String,
+  dust_concentration: String,
+  air_purity: String
+}
+
+impl Reading {
+    fn new(
+        tempurature: String,
+        humidity: String,
+        pressure: String,
+        dust_concentration: String,
+        air_purity: String
+    ) -> Reading {
+        Reading {
+            tempurature: tempurature,
+            humidity: humidity,
+            pressure: pressure,
+            dust_concentration: dust_concentration,
+            air_purity: air_purity
+        }
+    }
+}
+
+enum AirPurity {
+    Dangerous,
+    High,
+    Low,
+    FreshAir
+}
+
+impl AirPurity {
+    fn from_value(value: u16) -> String {
+        let result = match value {
+            0..=50 => AirPurity::FreshAir,
+            51..=100 => AirPurity::Low,
+            101..=150 => AirPurity::High,
+            151.. => AirPurity::Dangerous,
+        };
+       result.to_string()
+    }
+}
+
+// implements fmt::Display for AirPurity so that we can call .to_string() on
+// each enum value
+impl fmt::Display for AirPurity {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AirPurity::Low => write!(f, "LOW"),
+            AirPurity::High => write!(f, "HIGH"),
+            AirPurity::Dangerous => write!(f, "DANGEROUS"),
+            AirPurity::FreshAir => write!(f, "FRESH_AIR")
+        }
+    }
+}
+
+fn random_gen_humidity() -> String {
+    let mut rng = thread_rng();
+    let value = rng.gen_range(0.0..=100.0);
+    format!("{:.1}", value)
+}
+
+fn random_gen_tempurature() -> String {
+    let mut rng = thread_rng();
+    let value = rng.gen_range(15.0..=35.0);
+    format!("{:.1}", value)
+}
+
+fn random_gen_pressure() -> String {
+  let mut rng = thread_rng();
+  rng.gen_range(900..=1100).to_string()
+}
+
+fn random_gen_dust_concentration() -> String {
+    let mut rng = thread_rng();
+    rng.gen_range(0..=1000).to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+   let dust_concentration = random_gen_dust_concentration();
+   let air_purity = AirPurity::from_value(dust_concentration.parse::<u16>().unwrap());
+   let reading = Reading::new(
+       random_gen_tempurature(),
+       random_gen_humidity(),
+       random_gen_pressure(),
+       dust_concentration,
+       air_purity
+   );
+
+   let json = serde_json::to_string(&reading).unwrap();
+
+   println!("{}", json);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use regex::Regex;
+
+    #[test]
+    fn random_gen_humidity_returns_correctly_formatted_humidity_data() {
+      let result = random_gen_humidity();
+      let regex = Regex::new(r"\d{1,2}.\d{1}").unwrap();
+
+      assert!(regex.is_match(&result));
+    }
+    
+    #[test]
+    fn random_gen_tempurature_returns_correctly_formatted_humidity_data() {
+      let result = random_gen_tempurature();
+      let regex = Regex::new(r"\d{1,2}.\d{1}").unwrap();
+
+      assert!(regex.is_match(&result));
+    }
+
+    #[test]
+    fn random_gen_pressure_returns_correctly_formatted_pressure_data() {
+        let result = random_gen_pressure();
+        let regex = Regex::new(r"\d{3,4}").unwrap();
+        assert!(regex.is_match(&result));
+    }
+
+    #[test]
+    fn random_gen_dust_concentration_returns_correctly_formatted_pressure_data() {
+        let result = random_gen_dust_concentration();
+        let regex = Regex::new(r"\d{0,4}").unwrap();
+        assert!(regex.is_match(&result));
+    }
+
+    #[test]
+    fn air_purity_from_value_returns_correct_enum() {
+        let mut rng = thread_rng();
+        let fresh_air = rng.gen_range(0..=50);
+        let low = rng.gen_range(51..=100);
+        let high = rng.gen_range(101..=150);
+        let dangerous = rng.gen_range(151..u16::MAX);
+
+        assert_eq!(AirPurity::from_value(fresh_air), "FRESH_AIR");
+        assert_eq!(AirPurity::from_value(low), "LOW");
+        assert_eq!(AirPurity::from_value(high), "HIGH");
+        assert_eq!(AirPurity::from_value(dangerous), "DANGEROUS");
+    }
 }
