7eb9a970835c905cfc65d7d5ae73c65031b4e350
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2023-02-17 01:20:17 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-//! # Provides a mock Ambi client that emulates real sensor hardware such as 
+//! # Provides a mock Ambi client that emulates real sensor hardware such as
 //! an Edge client.
 //!
 //! This application emulates a real set of hardware sensors that can report on
 //! environmental conditions such as temperature, pressure, humidity, etc.
-//! 
+//!
 //! Please see the `ambi` repository for the web backend that this client connects to
 //! and the `edge-rs` repository for what this client is emulating.
 //!
 //! See the `LICENSE` file for Copyright and license details.
-//! 
+//!
 
-use clap::{Parser};
+use clap::Parser;
 
 fn main() {
     // Parses the provided command line interface arguments and flags
@@ -18,8 +18,8 @@ fn main() {
 
     match cli.debug {
         true => println!("Debug mode is now *on*"),
-        false => println!("Debug mode is now *off*")
+        false => println!("Debug mode is now *off*"),
     }
-    
+
     ambi_mock_client::run(&cli);
 }
